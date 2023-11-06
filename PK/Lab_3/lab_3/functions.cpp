5a81e0e6161266603802ea7ce13333bbd255eb5e
karlikp <pitera.karol@gmail.com>
2023-11-06 16:52:33 +0100

    push to PC

---
+++ PK/Lab_3/lab_3/functions.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+void zad_1()
+{
+	int arr[] = { 1, 2, 3, 4, 5 };
+
+	[](int )
+	{
+		for (int x : arr[])
+
+
+	}
+}
---
+++ PK/Lab_3/lab_3/functions.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+void zad_1();
+void zad_2();
+void zad_3();
